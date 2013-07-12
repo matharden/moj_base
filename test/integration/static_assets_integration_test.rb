e0ff4147b7813e37a5e2d07d3f886279442b8805
Mat Harden <matharden@gmail.com>
2013-07-12 15:34:40 +0100

    Fixed integration tests

--- test/integration/static_assets_integration_test.rb
+++ test/integration/static_assets_integration_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 describe "moj base integration" do
-  it "provides moj_base.js on the asset pipeline" do
-    visit '/assets/moj_base.js'
-    page.text.must_include 'var MOJBase ='
+  it "provides moj.js on the asset pipeline" do
+    visit '/assets/moj.js'
+    page.text.must_include 'var moj ='
   end
 
   it "provides moj_base.css on the asset pipeline" do
-    visit '/assets/moj_base.css'
-    page.text.must_include '.moj_base {'
+    visit '/assets/moj.css'
+    page.text.must_include 'moj'
   end
 end
