aa359eac3d3489c5c67b8fc3ddb3ee14cc2adbe5
Mat Harden <matharden@gmail.com>
2013-07-12 15:45:20 +0100

    Removed initializer

--- lib/moj_base/engine.rb
+++ lib/moj_base/engine.rb
@@ -1,7 +1,4 @@
 module MojBase
   class Engine < ::Rails::Engine
-    initializer 'moj_base.load_static_assets' do |app|
-      app.middleware.use ::ActionDispatch::Static, "#{root}/app"
-    end
   end
 end
